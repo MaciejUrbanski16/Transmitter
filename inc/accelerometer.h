9b1a6c0b7541b558783377142650dd1aaf1c3e28
Maciej U <urbanskimac@gmail.com>
2022-02-14 20:32:18 +0100

    Reading data from accelerometer
    - data as m/s2
    - avoiding floating point displaying on LCD

--- inc/accelerometer.h
+++ inc/accelerometer.h
@@ -16,11 +16,22 @@
 #define LIS3DSH_CTRL_REG5_ADDR               0x24
 
 #define dataRate400Hz 0b01110111 //0x77 dataRate -> 400Hz and enable X Y Z axis
-#define antiAliasing  0b01001000 // 400Hz anti aliasing filter bandwith | full scale selection 4G
+#define antiAliasing  0b00010000 // 400Hz anti aliasing filter bandwith | full scale selection 4G
+
+extern const float g;
+extern const int rawGrawity;
+
+typedef struct{
+	int integerPart;
+	int floatingPart;
+}AccelerationMS2;
 
 extern I2C_HandleTypeDef hi2c1;
 
 void initAccelerometer(void);
+float calculateAcceleration(const int rawAcceleration);
+
+AccelerationMS2 getAcceleration(const float floatingAcceleration);
 
 
 
--- src/accelerometer.c
+++ src/accelerometer.c
@@ -1,5 +1,7 @@
 #include "accelerometer.h"
 
+const float g = 9.803;
+const int rawGrawity = 5450;
 
 void initAccelerometer()
 {
@@ -11,4 +13,22 @@ void initAccelerometer()
 	HAL_I2C_Mem_Write(&hi2c1, ACC_ADDRESS << 1, LIS3DSH_CTRL_REG4_ADDR, 1, &array[0], 1, 100);
 	//anti aliasing filter 400Hz
 	HAL_I2C_Mem_Write(&hi2c1, ACC_ADDRESS << 1, LIS3DSH_CTRL_REG5_ADDR, 1, &array[1], 1, 100);
+}
+
+float calculateAcceleration(const int rawAcceleration)
+{
+	return ((float)rawAcceleration * g)/(float)rawGrawity;
+}
+
+AccelerationMS2 getAcceleration(const float floatingAcceleration)
+{
+	AccelerationMS2 acceleration;
+
+	int integerPart = (int)floatingAcceleration;
+	int floatingPart = (int)((float)(floatingAcceleration - integerPart) * 1000);
+
+	acceleration.integerPart = integerPart;
+	acceleration.floatingPart = floatingPart;
+
+	return acceleration;
 }
